31cdc1b10d05f0f0f1a9f5a0c9da1d991a5238fc
Y7T007 <yassir7t@gmail.com>
2024-02-21 19:49:36 +0100

    dashboard elements  fixed

--- login.php
+++ login.php
@@ -26,15 +26,29 @@
                                     <div class="text-center">
                                         <h4 class="text-dark mb-4">Welcome Back!</h4>
                                     </div>
-                                    <form class="user">
-                                        <div class="mb-3"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email"></div>
-                                        <div class="mb-3"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password"></div>
+                                    <?php
+                                    session_start();
+                                    if (isset($_SESSION['login_error'])) {
+                                        echo '<div class="alert alert-danger">' . $_SESSION['login_error'] . '</div>';
+                                        unset($_SESSION['login_error']);
+                                    }
+                                    ?>
+                                    <form class="user" method="post" action="login_process.php">
+                                        <div class="mb-3">
+                                            <input class="form-control form-control-user" type="text" id="inputCompteurID" placeholder="Enter Compteur ID..." name="compteurID">
+                                        </div>
+                                        <div class="mb-3">
+                                            <input class="form-control form-control-user" type="date" id="inputDateNaissance" placeholder="Enter Date of Birth..." name="dateNaissance">
+                                        </div>
                                         <div class="mb-3">
                                             <div class="custom-control custom-checkbox small">
-                                                <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label></div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1">
+                                                    <label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label>
+                                                </div>
                                             </div>
-                                        </div><button class="btn btn-primary d-block btn-user w-100" type="submit">Login</button>
-                                        <hr><a class="btn btn-primary d-block btn-google btn-user w-100 mb-2" role="button"><i class="fab fa-google"></i>&nbsp; Login with Google</a><a class="btn btn-primary d-block btn-facebook btn-user w-100" role="button"><i class="fab fa-facebook-f"></i>&nbsp; Login with Facebook</a>
+                                        </div>
+                                        <button class="btn btn-primary d-block btn-user w-100" type="submit">Login</button>
                                         <hr>
                                     </form>
                                     <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</a></div>
