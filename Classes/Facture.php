6420d25a81cb5a58ac4e7c499b91bdfad3764fff
Y7T007 <yassir7t@gmail.com>
2024-03-06 01:18:56 +0100

    Facture fixing naming conventions

--- Classes/Facture.php
+++ Classes/Facture.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Classes;
+
 use PDO;
 use PDOException;
 
@@ -8,30 +9,30 @@ require_once 'vendor/autoload.php';
 
 class Facture
 {
-    public $FactureID;
-    public $CompteurID;
-    public $DateFacture;
-    public $Consomation;
-    public $DateLimite;
-    public $Statut;
-    public $DatePaiement;
-    public $DateCreation;
-    public $CreatedBy;
-    public $Image;
+    public $factureId;
+    public $compteurId;
+    public $dateFacture;
+    public $consomation;
+    public $dateLimite;
+    public $statut;
+    public $datePaiement;
+    public $dateCreation;
+    public $createdBy;
+    public $image;
 
     private $pdo;
 
-    public function __construct($CompteurID, $DateFacture, $Consomation, $DateLimite, $Statut, $DatePaiement, $DateCreation, $CreatedBy, $Image = null)
+    public function __construct($compteurId, $dateFacture, $consomation, $dateLimite, $statut, $datePaiement, $dateCreation, $createdBy, $image = null)
     {
-        $this->CompteurID = $CompteurID;
-        $this->DateFacture = $DateFacture;
-        $this->Consomation = $Consomation;
-        $this->DateLimite = $DateLimite;
-        $this->Statut = $Statut;
-        $this->DatePaiement = $DatePaiement;
-        $this->DateCreation = $DateCreation;
-        $this->CreatedBy = $CreatedBy;
-        $this->Image = $Image;
+        $this->compteurId = $compteurId;
+        $this->dateFacture = $dateFacture;
+        $this->consomation = $consomation;
+        $this->dateLimite = $dateLimite;
+        $this->statut = $statut;
+        $this->datePaiement = $datePaiement;
+        $this->dateCreation = $dateCreation;
+        $this->createdBy = $createdBy;
+        $this->image = $image;
         $DB_connection = new DB_connection();
         $this->pdo = $DB_connection->getPDO();
     }
@@ -41,13 +42,14 @@ class Facture
         $sql = "INSERT INTO `Facture` (`CompteurID`, `DateFacture`, `Consomation`, `DateLimite`, `Statut`, `DatePaiement`, `DateCreation`, `CreatedBy`, `Image`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$this->CompteurID, $this->DateFacture, $this->Consomation, $this->DateLimite, $this->Statut, $this->DatePaiement, $this->DateCreation, $this->CreatedBy, $this->Image]);
+        $stmt->execute([$this->compteurId, $this->dateFacture, $this->consomation, $this->dateLimite, $this->statut, $this->datePaiement, $this->dateCreation, $this->createdBy, $this->image]);
     }
-    public function getFacture($factureID)
+
+    public function getFacture($factureId)
     {
         $sql = "SELECT * FROM `Facture` WHERE `FactureID` = ?";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$factureID]);
+        $stmt->execute([$factureId]);
         return $stmt->fetch();
     }
 
@@ -74,57 +76,63 @@ class Facture
         }
     }
 
-    public function getAnnualFacture($compteurID, $year)
+    public function getAnnualFacture($compteurId, $year)
     {
         $sql = "SELECT SUM(`Consomation`) as annualFacture FROM `Facture` WHERE `CompteurID` = ? AND YEAR(`DateFacture`) = ?";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$compteurID, $year]);
+        $stmt->execute([$compteurId, $year]);
         return $stmt->fetch()['annualFacture'];
     }
-    public function getAnnualFacturation($compteurID, $year)
+
+    public function getAnnualFacturation($compteurId, $year)
     {
         $sql = "SELECT * FROM `Facture` WHERE `CompteurID` = ? AND YEAR(`DateFacture`) = ?";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$compteurID, $year]);
+        $stmt->execute([$compteurId, $year]);
         $result = $stmt->fetchAll();
         return $result ? $result : []; // Return an empty array if there are no matching records
     }
-    public function getAvailableFactureToPay($compteurID)
+
+    public function getAvailableFactureToPay($compteurId)
     {
         $sql = "SELECT * FROM `Facture` WHERE `CompteurID` = ?";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$compteurID]);
+        $stmt->execute([$compteurId]);
         return $stmt->fetchAll();
     }
-    public function getUnpaidFactures($compteurID)
+
+    public function getUnpaidFactures($compteurId)
     {
         $sql = "SELECT * FROM `Facture` WHERE `CompteurID` = ? AND `Statut` = 'waiting'";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$compteurID]);
+        $stmt->execute([$compteurId]);
         return $stmt->fetchAll();
     }
-    public function updateFacture($Consomation, $Statut, $Image,$FactureID)
+
+    public function updateFacture($consomation, $statut, $image, $factureId)
     {
         try {
             $sql = "UPDATE `Facture` SET `Consomation` = ?, `Statut` = ?, `Image` = ? WHERE `FactureID` = ?";
             $stmt = $this->pdo->prepare($sql);
-            $stmt->execute([$Consomation, $Statut, $Image, $FactureID]);
+            $stmt->execute([$consomation, $statut, $image, $factureId]);
         } catch (PDOException $e) {
             // Handle the exception appropriately (log, show error page, etc.)
             throw new PDOException($e->getMessage(), (int)$e->getCode());
         }
     }
-    public function updateFactureStatus($factureID, $status)
+
+    public function updateFactureStatus($factureId, $status)
     {
         $sql = "UPDATE `Facture` SET `Statut` = ? WHERE `FactureID` = ?";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$status, $factureID]);
+        $stmt->execute([$status, $factureId]);
     }
-    public function getUserFactures($compteurID)
+
+    public function getUserFactures($compteurId)
     {
         $sql = "SELECT * FROM `Facture` WHERE `CompteurID` = ?";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$compteurID]);
+        $stmt->execute([$compteurId]);
         return $stmt->fetchAll();
     }
 }
